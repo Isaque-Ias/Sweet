f62ac616a04964eab21a23407c9a6471e6533dac
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2026-04-27 19:41:09 -0300

    Game

--- app/main.py
+++ app/main.py
@@ -1,84 +1,47 @@
 import sweet as sw
-from sweet.graphics.texture import Texture
-from sweet.entity import EntityTools, Entity
-from sweet.inputting import Input
+from sweet.entity import *
+from sweet.inputting import *
+from sweet.graphics.texture import *
 from pathlib import Path
-from sweet.linalg.vector import Vec
 from pygame.locals import *
-from math import cos, radians
 
 SOURCE = Path.cwd() / "app" / "sources"
-sw.looping.GameLoop.set_title("Assobi")
-height, width = sw.looping.GameLoop.view_width, sw.looping.GameLoop.view_height
-sw.looping.GameLoop.set_screen_size((height, width))
-
+sw.looping.GameLoop.set_screen_size((sw.looping.GameLoop.view_width, sw.looping.GameLoop.view_height))
 sw.init()
 
-Texture.set_texture("Pixel", SOURCE / "build" / "pixel.png")
-Texture.get_texture("Pixel").upload()
-Texture.set_texture("icon", SOURCE / "ceu.jpg")
-icon = Texture.get_texture("icon")
-sw.looping.GameLoop.set_icon(icon.get_image())
+Texture.set_texture("PlayerBody", SOURCE / "player_body.png").upload()
+Texture.set_texture("PlayerLeg", SOURCE / "player_leg.png").upload()
 
-def sign(x):
-    return (x > 0) - (x < 0)
-
-class Plant(Entity):
+class Player(Entity):
     def __init__(self, pos):
         super().__init__(pos, order=5, tick=True)
-        self.vertices = [0, 0, 10, 0, 0, 0, 10, 20, 0, 0, 0, 0, 20, 20, 20, 20, 20, 100]
-        self.image = Texture.get_texture("Pixel")
-        self.length = 200
-        self.thickness = 10
-        self.time = 0
-        self.torsion = 50
-        self.mode = "idle"
-        self.pose = [(1, 180)]
+        self.spr_body = Texture.get_texture("PlayerBody")
+        self.spr_legs = Texture.get_texture("PlayerLeg")
+        self.size = 0.2
+        self.left_leg_angle = [0, 25]
+        self.right_leg_angle = [0, -25]
 
     def tick(self):
-        if self.mode == "idle":
-            self.time += 1
-            self.vertices[0] = cos(radians(self.time * 4)) * 50
-            for i in range(1, len(self.vertices)):
-                self.vertices[i] += (self.vertices[i - 1] - self.vertices[i]) / 5
-
-        if self.mode == "pose":
-            current_pose = 0
-            old_pos = Vec(0, 0)
-            total_vertices = len(self.vertices)
-            for i in range(len(self.vertices)):
-                angle = self.vertices[i] - 90
-                new_pos = old_pos + Vec((self.length / (total_vertices - 1)), 0).rotate(angle)
-
-                percentage = i / len(self.vertices)
-                if percentage > self.pose[current_pose][0] and current_pose < len(self.pose):
-                    current_pose += 1
-
-                goal_pos = Vec(self.pose[current_pose][0] * self.length, 0).rotate(self.pose[current_pose][1])
-                perfect_angle = -(goal_pos - new_pos).angle()
-                print(goal_pos, i, new_pos, perfect_angle)
-                self.vertices[i] += (perfect_angle - self.vertices[i]) / 10
-
-                old_pos = new_pos
-
-        for i in range(len(self.vertices)):
-            if abs(self.vertices[i]) > self.torsion:
-                self.vertices[i] -= sign(self.vertices[i]) * (abs(self.vertices[i]) - self.torsion) / 3
+        self.left_leg_angle[0] = (Vec(*pg.mouse.get_pos()) - self.pos).angle() + 90
 
     def draw(self):
-        old_pos = self.pos
-        total_vertices = len(self.vertices)
-        for i in range(total_vertices):
-            angle = self.vertices[i] - 90
-            new_pos = old_pos + Vec((self.length / (total_vertices - 1)), 0).rotate(angle)
+        body_width, body_height = self.spr_body.get_width() * self.size, self.spr_body.get_height() * self.size
+        leg_width, leg_height = self.spr_legs.get_width() * self.size, self.spr_legs.get_height() * self.size
+        
+        left_leg_center = Vec(.3 * body_width, .0 * body_height) + Vec(0, leg_height / 2).rotate(self.left_leg_angle[0])
+        right_leg_center = Vec(-.3 * body_width, .0 * body_height) + Vec(0, leg_height / 2).rotate(self.right_leg_angle[0])
+        
+        bottom_left_leg_center = Vec(.3 * body_width, .0 * body_height) + Vec(0, leg_height - leg_width / 2).rotate(self.left_leg_angle[0]) + Vec(0, leg_height / 2).rotate(self.left_leg_angle[1])
+        bottom_right_leg_center = Vec(-.3 * body_width, .0 * body_height) + Vec(0, leg_height - leg_width / 2).rotate(self.right_leg_angle[0]) + Vec(0, leg_height / 2).rotate(self.right_leg_angle[1])
+        
+        EntityTools.draw_image(self.spr_legs, (self.pos + left_leg_center).unp(), (leg_width, leg_height), self.left_leg_angle[0])
+        EntityTools.draw_image(self.spr_legs, (self.pos + bottom_left_leg_center).unp(), (leg_width, leg_height), self.left_leg_angle[1])
+        
+        EntityTools.draw_image(self.spr_body, self.pos.unp(), (body_width, body_height))
 
-            difference = (old_pos - new_pos)
-            distance = difference.magnitude()
-            EntityTools.draw_image(self.image, (new_pos / 2 + old_pos / 2).unp(), (distance + self.thickness, self.thickness), angle)
-            
-            old_pos = new_pos
+        EntityTools.draw_image(self.spr_legs, (self.pos + right_leg_center).unp(), (leg_width, leg_height), self.right_leg_angle[0])
+        EntityTools.draw_image(self.spr_legs, (self.pos + bottom_right_leg_center).unp(), (leg_width, leg_height), self.right_leg_angle[1])
 
-screen_size = Vec(*EntityTools.get_screen_size())
-Plant((screen_size / 2).unp())
+Player((200, 200))
 
 sw.start()
